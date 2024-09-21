63e4e0c6ef8d092723114b027a87a5c35ca97b17
MarcosFerreira2024 <marcosfp2021@hotmail.com>
2024-09-20 21:12:52 -0300

    hover  com java script

---
+++ script.js
@@ -0,0 +1,19 @@
+const textos = document.querySelectorAll(".texto");
+
+function mudar_link() {
+  this.classList.add("texto-click");
+}
+
+function add_hover() {
+  this.classList.add("texto-hover");
+}
+
+function remove_hover() {
+  this.classList.remove("texto-hover");
+}
+
+textos.forEach((element) => {
+  element.addEventListener("click", mudar_link);
+  element.addEventListener("mouseover", add_hover);
+  element.addEventListener("mouseout", remove_hover);
+});
