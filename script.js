7723d6417de2f908d1be7ab7fdf056772be57652
MarcosFerreira2024 <marcosfp2021@hotmail.com>
2024-09-24 01:49:33 -0300

    galeria js

--- script.js
+++ script.js
@@ -19,3 +19,34 @@ textos.forEach((element) => {
   element.addEventListener("mouseout", remove_hover);
 });
 
+// galeria \\
+
+const galeria = document.querySelectorAll("#galeria img");
+const principal = document.querySelector("#imagem-principal");
+const botaoNavegar = document.querySelector("#botao-galeria");
+
+function selecionar_item(event) {
+  const src = event.currentTarget.src;
+  const alt = event.currentTarget.alt;
+  const url = event.currentTarget.dataset.url;
+
+  if (principal.src !== src) {
+    principal.classList.add("fade-out");
+
+    setTimeout(() => {
+      principal.src = src;
+      principal.alt = alt;
+      botaoNavegar.href = url;
+      principal.classList.remove("fade-out");
+      principal.classList.add("fade-in");
+    }, 300);
+
+    setTimeout(() => {
+      principal.classList.remove("fade-in");
+    }, 600);
+  }
+}
+
+galeria.forEach((img) => {
+  img.addEventListener("click", selecionar_item);
+});
